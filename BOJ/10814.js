f5fcafba4c265b1d446008b5ca2287a77258b2ed
hamdain <dain9812@naver.com>
2022-05-11 18:08:23 +0900

    Add 10814 solution file

---
+++ BOJ/10814.js
@@ -0,0 +1,8 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+let n = input.shift();
+
+input.sort((a, b) => a.split(" ")[0] - b.split(" ")[0]);
+
+console.log(input.join("\n"));
